7b1e46f281c0c1aeaa40f2e9f0d8278658f7d788
Gabriel <gabriel@rethink.ws>
2019-03-19 04:44:45 -0300

    change baseURL

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const Client = axios.create({
-  baseURL: 'http://localhost:3080',
+  baseURL: 'https://music-apps.herokuapp.com/',
   crossDomain: true,
   headers: { 'Content-Type': 'application/json' }
 });
