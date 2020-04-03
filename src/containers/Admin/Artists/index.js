88a0aa7e1a53df3da7f2b0e82555484158eb40a0
mateus <mmdfmateus@gmail.com>
2020-04-02 23:21:52 -0300

    migrate admin/artists

--- src/containers/Admin/Artists/index.js
+++ src/containers/Admin/Artists/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import '../../../App.css';
 import { Container, Col, Table, Button, Modal, I } from 'react-bootstrap';
 import Footer from '../../../components/Footer';
@@ -7,92 +7,74 @@ import Header from '../../../components/Header';
 import { Wrapper, FormWrapper } from './styled';
 import { FormControl } from 'react-bootstrap';
 
-class AdminArtists extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            artists: [],
-            show: false,
-            artistName: '',
-            artistUrl: ''
-        };
-      }
+export default function AdminArtists() {
+  const [artists, setArtists] = useState([]);
+  const [show, setShow] = useState(false);
+  const [artistName, setArtistName] = useState('');
+  const [artistUrl, setArtistUrl] = useState('');
 
-    componentDidMount() {
-        fetchAllArtists().then(({data}) => {
-            this.setState({ artists: data })
-        });
-    }
+  useEffect(() => {
+    fetchAllArtists().then(({ data }) => {
+      setArtists(data);
+    });
+  });
 
-    toggleModal = () => {
-      this.setState(prevState => ({
-        show: !prevState.show
-      }))
-    }
+  const toggleModal = () => setShow(!show);
 
-    createArtist = () => {
-      const { artistName, artistUrl } = this.state;
-      createArtist(artistName, artistUrl);
-      this.toggleModal();
-    }
+  const createArtist = () => {
+    createArtist(artistName, artistUrl);
+    this.toggleModal();
+  }
 
-    render() {
-        const { artists, show } = this.state;
-        return (
-            <Fragment>
-                <Header />
-                <Container>
-                    <Col>
-                    <Wrapper>
-                      <Button style={{ float: 'right', margin: 4 }} onClick={this.toggleModal}>Criar</Button>
-                      <Table striped bordered hover>
-                        <thead>
-                          <tr>
-                            <th>Id</th>
-                            <th>Nome</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {artists.map(artist => (<tr><td>{artist._id}</td><td>{artist.name}</td></tr>))}
-                        </tbody>
-                      </Table>
-                      </Wrapper>
-                    </Col>
-                </Container>
-                <Footer />
-                <Modal show={show} >
-                  <Modal.Header>
-                    <Modal.Title>Criar Artista</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                    <FormWrapper>
-                      <FormControl
-                        placeholder="Nome do Artista"
-                        aria-label="Artista"
-                        aria-describedby="basic-addon1"
-                        onChange={e => this.setState({ artistName: e.target.value })}
-                        style={{ marginBottom: 12 }}
-                      />
-                      <FormControl
-                        placeholder="Imagem do Artista"
-                        aria-label="Imagem"
-                        aria-describedby="basic-addon1"
-                        onChange={e => this.setState({ artistUrl: e.target.value })}
-                      />
-                    </FormWrapper>
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={this.toggleModal}>
-                      Cancelar
-                    </Button>
-                    <Button variant="primary" onClick={this.createArtist}>
-                      Criar
-                    </Button>
-                  </Modal.Footer>
-                </Modal>
-            </Fragment>
-        );
-    }
+  return (
+    <Fragment>
+      <Header />
+      <Container>
+        <Col>
+          <Wrapper>
+            <Button style={{ float: 'right', margin: 4 }} onClick={toggleModal}>Criar</Button>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Nome</th>
+                </tr>
+              </thead>
+              <tbody>
+                {artists.map(artist => (<tr><td>{artist._id}</td><td>{artist.name}</td></tr>))}
+              </tbody>
+            </Table>
+          </Wrapper>
+        </Col>
+      </Container>
+      <Footer />
+      <Modal show={show} >
+        <Modal.Header>
+          <Modal.Title>Criar Artista</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormWrapper>
+            <FormControl
+              placeholder="Nome do Artista"
+              aria-label="Artista"
+              aria-describedby="basic-addon1"
+              onChange={e => setArtistName(e.target.value)}
+              style={{ marginBottom: 12 }}
+            />
+            <FormControl
+              placeholder="Imagem do Artista"
+              aria-label="Imagem"
+              aria-describedby="basic-addon1"
+              onChange={e => setArtistUrl(e.target.value)}
+            />
+          </FormWrapper>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={toggleModal}>Cancelar</Button>
+          <Button variant="primary" onClick={createArtist}>Criar</Button>
+        </Modal.Footer>
+      </Modal>
+    </Fragment>
+  );
+
 }
-
-export default AdminArtists;
