89ec941435a243704a6086dc5bff1026ed2784c8
mateus <mmdfmateus@gmail.com>
2020-04-02 22:53:15 -0300

    migrate list artist and song

--- src/containers/Home/components/ListArtist/index.js
+++ src/containers/Home/components/ListArtist/index.js
@@ -1,29 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Image, ArtistName, ArtistWrapper } from './styled';
 
-class ListArtist extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isSuggestion: false,
-            artists: []
-        }
-      }
-
-    render() {
-        const { artist } = this.props;
-        return (
-            <ArtistWrapper>
-                <Link to={{ pathname: '/artists', state: { artistId: artist._id} }}>
+export default function ListArtist({ artist }) {
+    return (
+        <ArtistWrapper>
+            <Link to={{ pathname: '/artists', state: { artistId: artist._id } }}>
                 <Image src={artist.image} />
                 <ArtistName>
                     {artist.name}
                 </ArtistName>
-                </Link>
-            </ArtistWrapper>
-        )
-    }
+            </Link>
+        </ArtistWrapper>
+    )
 }
-
-export default ListArtist;
--- src/containers/Home/components/ListSong/index.js
+++ src/containers/Home/components/ListSong/index.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { SongName, SongWrapper, ArtistName } from './styled';
 
-class ListSong extends Component {
-
-    render() {
-        const { song } = this.props;
-        return (
-            <SongWrapper>
-                <Link to={{ pathname: '/songs', state: { songId: song._id} }}>
+export default function ListSong({ song }) {
+    return (
+        <SongWrapper>
+            <Link to={{ pathname: '/songs', state: { songId: song._id } }}>
                 <SongName>
                     {song.name}
                 </SongName>
                 <ArtistName>
                     {song.artistName}
                 </ArtistName>
-                </Link>
-            </SongWrapper>
-        )
-    }
+            </Link>
+        </SongWrapper>
+    )
 }
-
-export default ListSong;
