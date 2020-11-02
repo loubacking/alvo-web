0f20de64b8836f9e4c06f2cc279dd562816e8af1
louback <gabriel@rethink.dev>
2020-11-02 02:03:20 -0300

    changing api url

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const Client = axios.create({
-  baseURL: 'https://music-apps.herokuapp.com/',
+  baseURL: 'https://alvocifras-api.herokuapp.com/',
   crossDomain: true,
   headers: { 'Content-Type': 'application/json', 'temporary-access': 'alvo2020' }
 });
