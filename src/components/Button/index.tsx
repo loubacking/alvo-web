8ecf5dda31ea2fd55bc87991f0ae1e1ec462beae
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-07-17 03:53:30 -0300

    feat: add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { SignInButton, TitleButton } from './styled';
+
+export type ButtonProps = {
+  title: string;
+  fontSize?: number;
+  color?: string;
+  background?: string;
+  hoverBackground?: string;
+  onClick: () => void;
+};
+
+const Button = ({
+  title,
+  fontSize,
+  color,
+  background,
+  hoverBackground,
+  onClick,
+}: ButtonProps) => (
+  <SignInButton
+    onClick={onClick}
+    background={background}
+    hoverBackground={hoverBackground}
+  >
+    <TitleButton fontSize={fontSize} color={color}>
+      {title}
+    </TitleButton>
+  </SignInButton>
+);
+
+export default Button;
