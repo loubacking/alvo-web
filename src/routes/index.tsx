8a52cf2e48ccb444f51575fd2c71bbeb3e42af87
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-07-18 01:17:28 -0300

    feat: add /register route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import Home from '../containers/Home';
+import Register from '../containers/Register';
 import Artists from '../containers/Artists';
 import Songs from '../containers/Songs';
 import AdminArtists from '../containers/Admin/Artists';
@@ -11,12 +12,13 @@ const Routes: React.FC = () => {
   return (
     <>
       <Route exact path="/" component={Home} />
+      <Route exact path="/register" component={Register} />
       <Route exact path="/artists" component={Artists} />
       <Route exact path="/songs" component={Songs} />
       <Route exact path="/admin/artists" component={AdminArtists} />
       <Route exact path="/admin/songs" component={AdminSongs} />
     </>
   );
-}
+};
 
 export default Routes;
