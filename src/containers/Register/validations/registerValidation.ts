cf34e13437ca6ff63323916c74c19a339c8b14f3
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-07-19 02:24:13 -0300

    feat: add validation name

---
+++ src/containers/Register/validations/registerValidation.ts
@@ -0,0 +1,25 @@
+import { REGEX_REGULAR_NAME, REGEX_SPECIAL_CHARACTERS } from "../constants/constants"
+
+function validationName(name:string): boolean {
+  const nameTrim = name.trim()
+
+  if(nameTrim.length == 0) {
+    return false
+  }
+
+  if(nameTrim.match(REGEX_REGULAR_NAME)) {
+    return false
+  }
+
+  if(nameTrim.match(REGEX_SPECIAL_CHARACTERS)){
+    return false
+  }
+
+  return true
+}
+
+const validationEmail = () => {
+
+}
+
+export {validationName, validationEmail}
